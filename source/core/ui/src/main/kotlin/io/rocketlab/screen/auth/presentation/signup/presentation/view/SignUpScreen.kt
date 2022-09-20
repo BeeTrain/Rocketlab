31783d270826b52815745ac1ff3615585a60f7aa
Max Chernakov <41rm4kc@gmail.com>
2022-09-20 18:32:32 +0300

    #000019 fix sign up button

--- source/core/ui/src/main/kotlin/io/rocketlab/screen/auth/presentation/signup/presentation/view/SignUpScreen.kt
+++ source/core/ui/src/main/kotlin/io/rocketlab/screen/auth/presentation/signup/presentation/view/SignUpScreen.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -39,10 +39,10 @@ import org.koin.androidx.compose.getViewModel
 fun SignUpScreen(
     viewModel: SignUpViewModel = getViewModel()
 ) {
+    val scaffoldState = rememberScaffoldState()
+
     val uiState by viewModel.uiState.collectAsState()
     val errorState by viewModel.errorState.collectAsState()
-
-    val scaffoldState = rememberScaffoldState()
 
     Scaffold(
         scaffoldState = scaffoldState
