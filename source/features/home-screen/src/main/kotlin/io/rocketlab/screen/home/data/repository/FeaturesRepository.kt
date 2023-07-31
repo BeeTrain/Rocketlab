4ab734a42ca02241f7f5adbaa195b0aecc6b2635
Max Chernakov <41rm4kc@gmail.com>
2023-07-31 11:52:33 +0300

    #000063 use entries in enums, fix background for header

--- source/features/home-screen/src/main/kotlin/io/rocketlab/screen/home/data/repository/FeaturesRepository.kt
+++ source/features/home-screen/src/main/kotlin/io/rocketlab/screen/home/data/repository/FeaturesRepository.kt
@@ -5,6 +5,6 @@ import io.rocketlab.screen.home.domain.model.Feature
 class FeaturesRepository {
 
     fun getFeatures(): List<Feature> {
-        return Feature.values().toList()
+        return Feature.entries
     }
 }
--- source/features/note-screen/src/main/kotlin/io/rocketlab/screen/note/list/presentation/view/note/NotesColumn.kt
+++ source/features/note-screen/src/main/kotlin/io/rocketlab/screen/note/list/presentation/view/note/NotesColumn.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -61,7 +60,7 @@ fun NotesColumn(
                     style = Typography.headlineMedium,
                     textAlign = TextAlign.Start,
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.background)
+                        .background(Color.Transparent)
                         .fillMaxWidth()
                 )
             }
